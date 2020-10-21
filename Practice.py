c939efd7f13cb5b1f296c0718d6923a7af77d109
Archangelo08 <73217747+Archangelo08@users.noreply.github.com>
2020-10-21 01:19:30 -0600

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,20 @@
+
+colors = ['black', 'white', 'yellow']
+sizes = ['small', 'medium', 'large']
+
+Tshirts = [(color, size) for size in sizes
+           for color in colors]
+
+print(Tshirts)
+
+
+
+
+
+
+
+
+
+
+
+
